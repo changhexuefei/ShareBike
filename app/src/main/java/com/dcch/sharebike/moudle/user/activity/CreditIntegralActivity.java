88416562273a29ca6086b66754bd465fdc6fac8a
changhexuefei <changhexuefei2011@163.com>
2017-02-10 13:05:32 +0800

    修改

--- app/src/main/java/com/dcch/sharebike/moudle/user/activity/CreditIntegralActivity.java
+++ app/src/main/java/com/dcch/sharebike/moudle/user/activity/CreditIntegralActivity.java
@@ -1,9 +1,17 @@
 package com.dcch.sharebike.moudle.user.activity;
 
+import android.widget.ImageView;
+
 import com.dcch.sharebike.R;
 import com.dcch.sharebike.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class CreditIntegralActivity extends BaseActivity {
+
+    @BindView(R.id.back)
+    ImageView back;
 
     @Override
     protected int getLayoutId() {
@@ -12,6 +20,13 @@ public class CreditIntegralActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+
+    }
+
+
+    @OnClick(R.id.back)
+    public void onClick() {
+        finish();
 
     }
 }
