37991d114296a96ded417cece621e5b0950a8529
gao <changhexuefei@163.com>
2017-02-13 22:56:56 +0800

    测试

---
+++ app/src/main/java/com/dcch/sharebike/moudle/user/activity/ChangeUserNickNameActivity.java
@@ -0,0 +1,19 @@
+package com.dcch.sharebike.moudle.user.activity;
+
+import com.dcch.sharebike.R;
+import com.dcch.sharebike.base.BaseActivity;
+
+public class ChangeUserNickNameActivity extends BaseActivity {
+
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_change_user_nick_name;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
