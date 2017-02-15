5054608fa3b58cfa2ad8d4f1a2a93e0f7e732d9b
changhexuefei <changhexuefei2011@163.com>
2017-02-15 17:56:09 +0800

    客户服务中无法开锁的页面的实现

--- app/src/main/java/com/dcch/sharebike/moudle/user/activity/CustomerServiceActivity.java
+++ app/src/main/java/com/dcch/sharebike/moudle/user/activity/CustomerServiceActivity.java
@@ -1,9 +1,21 @@
 package com.dcch.sharebike.moudle.user.activity;
 
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
 import com.dcch.sharebike.R;
 import com.dcch.sharebike.base.BaseActivity;
+import com.dcch.sharebike.moudle.user.fragment.UnableUnlockFragment;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class CustomerServiceActivity extends BaseActivity {
+
+    @BindView(R.id.back)
+    ImageView back;
+    @BindView(R.id.customerService)
+    FrameLayout customerService;
 
     @Override
     protected int getLayoutId() {
@@ -12,6 +24,13 @@ public class CustomerServiceActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+         getSupportFragmentManager().beginTransaction().add(R.id.customerService,new UnableUnlockFragment()).commit();
 
+    }
+
+
+    @OnClick(R.id.back)
+    public void onClick() {
+        finish();
     }
 }
--- app/src/main/java/com/dcch/sharebike/moudle/user/fragment/UnableUnlockFragment.java
+++ app/src/main/java/com/dcch/sharebike/moudle/user/fragment/UnableUnlockFragment.java
@@ -1,19 +1,40 @@
 package com.dcch.sharebike.moudle.user.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.dcch.sharebike.R;
+import com.dcch.sharebike.app.App;
+import com.louisgeek.multiedittextviewlib.MultiEditInputView;
+import com.xys.libzxing.zxing.activity.CaptureActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UnableUnlockFragment extends Fragment {
 
+
+    @BindView(R.id.bike_code)
+    TextView bikeCode;
+    @BindView(R.id.scan_code)
+    RelativeLayout scanCode;
+    @BindView(R.id.questionDesc)
+    MultiEditInputView questionDesc;
+    @BindView(R.id.confirm)
+    TextView confirm;
+    @BindView(R.id.tips)
+    TextView tips;
 
     public UnableUnlockFragment() {
         // Required empty public constructor
@@ -24,7 +45,32 @@ public class UnableUnlockFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_unable_unlock, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
 
+    @OnClick({R.id.scan_code, R.id.confirm})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.scan_code:
+                Intent i4 = new Intent(App.getContext(), CaptureActivity.class);
+                startActivityForResult(i4, 0);
+                break;
+            case R.id.confirm:
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == -1 && requestCode == 0) {
+            Bundle bundle = data.getExtras();
+            if (bundle != null) {
+                String result = bundle.getString("result");
+                bikeCode.setText(result);
+                tips.setVisibility(View.VISIBLE);
+
+            }
+        }
+    }
 }
