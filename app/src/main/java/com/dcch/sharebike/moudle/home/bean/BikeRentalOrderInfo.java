b6d7fa9185ef8a2c365123aea2f477f948f77d31
changhexuefei <changhexuefei2011@163.com>
2017-05-31 20:20:59 +0800

    ....

--- app/src/main/java/com/dcch/sharebike/moudle/home/bean/BikeRentalOrderInfo.java
+++ app/src/main/java/com/dcch/sharebike/moudle/home/bean/BikeRentalOrderInfo.java
@@ -31,26 +31,19 @@ public class BikeRentalOrderInfo {
     private String carRentalOrderId;
     private String carRentalOrderNo;
     private int userId;
-    private Object name;
-    private Object phone;
-    private Object nickName;
-    private Object idCard;
+
     private int bicycleId;
-    private int organization_ID;
-    private Object organization_No;
-    private Object organization_Name;
+
     private String bicycleNo;
     private int tripDist;
     private int tripTime;
     private int rideCost;
-    private Object carRentalOrderStatus;
+
     private String carRentalOrderDate;
-    private Object startTime;
-    private Object endTime;
+
     private String resultStatus;
     private int startPos;
-    private Object remark;
-    private Object returnBicycleDate;
+
 
     public String getCarRentalOrderId() {
         return carRentalOrderId;
@@ -76,37 +69,13 @@ public class BikeRentalOrderInfo {
         this.userId = userId;
     }
 
-    public Object getName() {
-        return name;
-    }
 
-    public void setName(Object name) {
-        this.name = name;
-    }
 
-    public Object getPhone() {
-        return phone;
-    }
 
-    public void setPhone(Object phone) {
-        this.phone = phone;
-    }
 
-    public Object getNickName() {
-        return nickName;
-    }
 
-    public void setNickName(Object nickName) {
-        this.nickName = nickName;
-    }
 
-    public Object getIdCard() {
-        return idCard;
-    }
 
-    public void setIdCard(Object idCard) {
-        this.idCard = idCard;
-    }
 
     public int getBicycleId() {
         return bicycleId;
@@ -114,30 +83,6 @@ public class BikeRentalOrderInfo {
 
     public void setBicycleId(int bicycleId) {
         this.bicycleId = bicycleId;
-    }
-
-    public int getOrganization_ID() {
-        return organization_ID;
-    }
-
-    public void setOrganization_ID(int organization_ID) {
-        this.organization_ID = organization_ID;
-    }
-
-    public Object getOrganization_No() {
-        return organization_No;
-    }
-
-    public void setOrganization_No(Object organization_No) {
-        this.organization_No = organization_No;
-    }
-
-    public Object getOrganization_Name() {
-        return organization_Name;
-    }
-
-    public void setOrganization_Name(Object organization_Name) {
-        this.organization_Name = organization_Name;
     }
 
     public String getBicycleNo() {
@@ -172,13 +117,6 @@ public class BikeRentalOrderInfo {
         this.rideCost = rideCost;
     }
 
-    public Object getCarRentalOrderStatus() {
-        return carRentalOrderStatus;
-    }
-
-    public void setCarRentalOrderStatus(Object carRentalOrderStatus) {
-        this.carRentalOrderStatus = carRentalOrderStatus;
-    }
 
     public String getCarRentalOrderDate() {
         return carRentalOrderDate;
@@ -188,21 +126,6 @@ public class BikeRentalOrderInfo {
         this.carRentalOrderDate = carRentalOrderDate;
     }
 
-    public Object getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Object startTime) {
-        this.startTime = startTime;
-    }
-
-    public Object getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(Object endTime) {
-        this.endTime = endTime;
-    }
 
     public String getResultStatus() {
         return resultStatus;
@@ -220,19 +143,4 @@ public class BikeRentalOrderInfo {
         this.startPos = startPos;
     }
 
-    public Object getRemark() {
-        return remark;
-    }
-
-    public void setRemark(Object remark) {
-        this.remark = remark;
-    }
-
-    public Object getReturnBicycleDate() {
-        return returnBicycleDate;
-    }
-
-    public void setReturnBicycleDate(Object returnBicycleDate) {
-        this.returnBicycleDate = returnBicycleDate;
-    }
 }
