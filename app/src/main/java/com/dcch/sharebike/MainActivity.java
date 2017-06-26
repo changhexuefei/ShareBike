aa3bfef3f9f97d01bc7ff3310ee46acd21edaa4c
changhexuefei <changhexuefei2011@163.com>
2017-06-26 23:00:41 +0800

    ....

--- app/src/main/java/com/dcch/sharebike/MainActivity.java
+++ app/src/main/java/com/dcch/sharebike/MainActivity.java
@@ -555,7 +555,6 @@ public class MainActivity extends BaseActivity implements BaiduMap.OnMapStatusCh
                         return;
                     }
                 }).setMsgColor(R.color.colorHeading).setMsgSize(16).show();
-
 //                if (isClick) {
 //                    Intent seek = new Intent(MainActivity.this, SeekActivity.class);
 //                    seek.putExtra("address", address1);
